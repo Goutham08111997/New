88ebc156570fc5ec887565d55dae3684332f7e83
goutham <gouthammass97@gmail.com>
2021-02-06 17:11:34 +0530

    created tycoon angular project

--- TycoonProjectRefactored/home.php
+++ TycoonProjectRefactored/home.php
@@ -5,17 +5,17 @@
   <title>Startup Growth Analyis</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="css/style.css">
+  
 </head>
 
-<body style="height:600px">
+<body>
 
-  <nav class="navbar navbar-expand-sm bg-info navbar-light sticky-top">
+  <nav class="navbar navbar-expand-sm bg-info navbar-light sticky-top" role="navigation">
     <a class="navbar-brand" href="#">
       <img src="img/Tycoon_logo.jpg" class="rounded-circle" alt="Logo" style="width:40px;">
       Tycoon
