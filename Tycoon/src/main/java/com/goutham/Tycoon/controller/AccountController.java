fd9fad22a8b2f3e0c5aa8729311b13fb5fca37df
goutham <gouthammass97@gmail.com>
2021-02-20 17:38:23 +0530

    modified apis

--- Tycoon/src/main/java/com/goutham/Tycoon/controller/AccountController.java
+++ Tycoon/src/main/java/com/goutham/Tycoon/controller/AccountController.java
@@ -3,28 +3,37 @@ package com.goutham.Tycoon.controller;
 import com.goutham.Tycoon.model.User;
 import com.goutham.Tycoon.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class AccountController {
 
     @Autowired
     AccountService accountService;
-    @PostMapping("/")
-    public User signup(@RequestBody User newUser){
-       User user= accountService.getByUserName(newUser.getUserName());
-       if(user!=null){
-         return  null;
-       }
-       else{
-           return  accountService.saveUser(user);
-       }
 
+    @PostMapping("/signup")
+    public User signup(@RequestBody User newUser) {
+        User user = accountService.getByUserName(newUser.getUserName());
+        if (user != null) {
+            return null;
+        } else {
+            return accountService.saveUser(user);
+        }
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        User requestedUser=accountService.getByUserName(user.getUserName());
 
-
+        if(requestedUser!=null){
+            if(requestedUser.getPassword().equals(user.getPassword())){
+                return "Success";
+            }
+            else
+                return "Invalid Password";
+        }
+        else
+            return   "Invalid user";
     }
 }
