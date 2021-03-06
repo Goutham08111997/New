da1fc19ef49292c1477f8080fde6b703838f1118
goutham <gouthammass97@gmail.com>
2021-03-06 09:03:50 +0530

    added logback xml ,added flyway properties ,added postgres dependency in pom.xml

--- Tycoon/src/main/resources/db/migration/V!__1.sql
+++
@@ -1,6 +0,0 @@
-create table user(
-id serial not null,
-user_name varchar(255),
-password varchar(255),
-confirm_password varchar(255)
-)
---
+++ Tycoon/src/main/resources/db/migration/V1__1.sql
@@ -0,0 +1,6 @@
+create table user(
+ id serial not null,
+ user_name varchar(255),
+ password varchar(255),
+ confirm_password varchar(255)
+)
